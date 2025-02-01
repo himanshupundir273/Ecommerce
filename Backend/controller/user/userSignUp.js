34d5df54aae34d1422152ceef74f96570da31e70
Pundir9711 <himanshu@asargaming.com>
2025-02-01 20:21:56 +0530

    changes done in this

--- Backend/controller/user/userSignUp.js
+++ Backend/controller/user/userSignUp.js
@@ -1,5 +1,5 @@
 const UserModel = require("../../models/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 async function userSignUpController(req, res) {
   try {
