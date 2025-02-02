bf1f569a28e3154312e569579439e6b54f96c8b2
Pundir9711 <himanshu@asargaming.com>
2025-02-02 17:29:54 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -2,25 +2,27 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
-const router = require("./routes")
-const cookieParser=require('cookie-parser')
+const router = require("./routes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(cors({
-  origin:'https://ecommerce-backend-mu-blue.vercel.app/',
-  methods:["GET","PUT","POST","DELETE"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/",(req,res)=>{
-    res.json({
-        message:"server is running"
-    });
-})
+app.get("/", (req, res) => {
+  res.json({
+    message: "server is running",
+  });
+});
 
-app.use("/api",router);
+app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 
